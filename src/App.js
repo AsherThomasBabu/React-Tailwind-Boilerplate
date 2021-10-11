d703a3143edf398dfa282e7339d5863bfe225a27
AsherThomasBabu <ashertb2000@gmail.com>
2021-10-11 18:21:15 +0530

    add basic jsx to see output

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return null;
+  return (
+    <>
+      <h1 className="text-4xl text-purple-600 text-center">
+        Hello Tailwind CSS
+      </h1>
+    </>
+  );
 }
 
 export default App;
